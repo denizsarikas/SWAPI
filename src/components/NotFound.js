de4ac7ed236fdcffdaf11591a96b789560895644
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-04-30 19:35:18 +0300

    notFound navigate update

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -8,13 +8,13 @@ const NotFound = () => {
   const navigate = useNavigate();
   const handleNavigate = (starship) => {
     // console.log('you clicked mee')
-    navigate(`/starships/`);
+    navigate(`/`);
   }
 
 
   return (
     <div className='notfoundpage'>
-      <div><h1>4</h1><img src={notfound}/><h1>4</h1></div>
+      <div><h1>4</h1><img src={notfound} alt='not-found'/><h1>4</h1></div>
       <h3>Great shot kid. That was one in a million.</h3>
       <button onClick={() =>handleNavigate()} className='button1'>LET'S GET YOU HOME</button>
     </div>
