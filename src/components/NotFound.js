0d0d42565471cbc682e1453c8ac22fd972229184
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-04-30 18:10:46 +0300

    404page update

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import notfound from '../assets/img/404.png'
+import { useNavigate } from "react-router-dom";
+
 
 const NotFound = () => {
+
+  const navigate = useNavigate();
+  const handleNavigate = (starship) => {
+    // console.log('you clicked mee')
+    navigate(`/starships/`);
+  }
+
+
   return (
     <div className='notfoundpage'>
-      <img src={notfound} />
+      <div><h1>4</h1><img src={notfound}/><h1>4</h1></div>
+      <h3>Great shot kid. That was one in a million.</h3>
+      <button onClick={() =>handleNavigate()} className='button1'>LET'S GET YOU HOME</button>
     </div>
   )
 }
